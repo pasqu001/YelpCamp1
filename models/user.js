c127775ea102d5612b28973797ed83c021642198
pasqu001 <kaseypasqualini@yahoo.com>
2019-12-30 15:03:02 -0500

    fixed routes/campground auth issue--found error in user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
-    usernmae: String,
+    username: String,
     password: String,
 });
 
